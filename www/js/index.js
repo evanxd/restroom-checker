85d43e79b10a5a0e92f13f883b393780a204ea1a
Evan Xd <itoyxd@gmail.com>
2015-08-20 11:58:23 +0800

    Do long polling for restroom checker service

--- www/js/index.js
+++ www/js/index.js
@@ -3,15 +3,24 @@
 
 (function() {
   var RESTROOM_API = "http://10.247.34.241:8080/restroom?callback=?";
+  var TIMER_INTERVAL = 100;
   var restroom1 = document.querySelector('#restroom1');
-  $.getJSON(RESTROOM_API, {
-    format: "json"
-  })
-  .done(function(data) {
-    if (data.rooms[0]) {
-      restroom1.style.backgroundColor = 'green';  
-    } else {
-      restroom1.style.backgroundColor = 'red';
-    }
-  });
+
+  setRestroomAvailability();
+
+  function setRestroomAvailability() {
+    $.getJSON(RESTROOM_API, {
+      format: "json"
+    })
+    .done(function(data) {
+      if (data.rooms[0]) {
+        restroom1.style.backgroundColor = 'green';  
+      } else {
+        restroom1.style.backgroundColor = 'red';
+      }
+      setTimeout(function() {
+        setRestroomAvailability(); 
+      }, TIMER_INTERVAL);
+    });
+  }
 }());
